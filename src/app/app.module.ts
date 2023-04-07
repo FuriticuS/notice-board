f68316e7e5a5f44a846a16fe033656d400444f7f
Fur1ouS <Fur1ouS@ya.ru>
2023-04-07 03:36:01 +0300

    add primeng styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import {HeaderComponent} from './components/header/header.component';
 import { SignComponent } from './components/sign/sign.component';
 import { SearchComponent } from './components/search/search.component';
 import { AdsListComponent } from './components/ads/ads-list/ads-list.component';
 import { AdCardComponent } from './components/ads/ad-card/ad-card.component';
+
+import {ButtonModule} from "primeng/button";
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { AdCardComponent } from './components/ads/ad-card/ad-card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
